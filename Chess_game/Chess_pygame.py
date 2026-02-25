458b7e107d6fc708a7b29ff502c7f3cbb020af49
PennyGuyDyls <dylanprothero@icloud.com>
2026-02-25 23:15:41 +0000

    Update Chess_pygame.py

--- Chess_game/Chess_pygame.py
+++ Chess_game/Chess_pygame.py
@@ -9,14 +9,14 @@ class game():
     def __init__(self):
         self.backboard = [[dark if (row + col) % 2 == 0 else light for col in range(8)] for row in range(8)]
 
-        top_bottom=[[rook(i),
+        top_bottom=[[rook(i,0,(0 if i else 7)),
                      knight(i,1,(0 if i else 7)),
-                     bishop(i),
-                     queen(i),
+                     bishop(i,2,(0 if i else 7)),
+                     queen(i,3,(0 if i else 7)),
                      king(i,4,(0 if i else 7)),
-                     bishop(i),
+                     bishop(i,5,(0 if i else 7)),
                      knight(i,6,(0 if i else 7)),
-                     rook(i)]
+                     rook(i,7,(0 if i else 7))]
                        for i in range(2)]
         self.board = [top_bottom[1],[pawn(1,i,1) for i in range(8)]]
 
@@ -31,43 +31,92 @@ class game():
                 if self.board[i][j]!=0:
                     self.board[i][j].rect.center=(j*100+50,i*100+50)
 
+        self.turn=0
         self.dots=[]
         self.action_piece=None
 
     def select(self,row,col):
+
         if [col,row] in self.dots:
             self.board=self.action_piece.move(self.board,row,col)
+            self.swap_turn()
             self.dots=[]
+
         elif self.board[row][col] == 0:
             self.dots=[]
-        elif isinstance(self.board[row][col],Piece):
+
+        elif isinstance(self.board[row][col],Piece) and self.board[row][col].colour==self.turn:
             self.action_piece=self.board[row][col]
             self.dots=self.board[row][col].check_possible_moves(self.board)
+
+    def swap_turn(self):
+        if self.turn:
+            self.turn=0
+        else:
+            self.turn=1
 
 
 class Piece(pygame.sprite.Sprite):
 
-    def check_valid_location(self,board,x,y,piece):
+    def check_valid_location(self,board,x,y):
         if not (0<=x<8 and 0<=y<8):
+            if self.type in ['BISHOP','ROOK','QUEEN']:
+                return False,False
             return False
         
         if board[y][x]==0:
-            if piece=='PAWN':
+            if self.type=='PAWN':
                 return False
+            if self.type in ['BISHOP','ROOK','QUEEN']:
+                return True,False
             return True
         
         if isinstance(board[y][x],Piece) and board[y][x].colour==self.oppcolour:
+            if self.type in ['BISHOP','ROOK','QUEEN']:
+                return True,True
             return True
+        
+        if self.type in ['BISHOP','ROOK','QUEEN']:
+            return False,True
+        return False
 
     def move(self,board,y,x):
-        self.move2=False
+        if self.type=='PAWN':
+            self.move2=False
+
+
+        elif self.type == 'KING':
+            self.moved=True
+            if x-self.posx==2:
+                board[self.posy][7].move(board,self.posy,5)
+            if x-self.posx==-2:
+                board[self.posy][0].move(board,self.posy,3)
+
+        elif self.type =='ROOK':
+            self.moved=True
+
         board[self.posy][self.posx]=0
         self.posx,self.posy=x,y
         self.rect.center = (x*100+50,y*100+50)
         board[y][x]=self
         return board
     
+    def checks_for_lines(self, board,directions):
+        dots=[]
 
+        for i in directions:
+            newx,newy=self.posx,self.posy
+            while 0<=newx<8 and 0<=newy<8:
+                newx+=i[0]
+                newy+=i[1]
+                free,piece=self.check_valid_location(board,newx,newy)
+                if free:
+                    dots.append([newx,newy])
+                if piece:
+                    break
+
+        return dots
+    
 
 class pawn(Piece):
     def __init__(self, colour,x,y):
@@ -77,6 +126,7 @@ class pawn(Piece):
         self.image=pieces[colour][0]
         self.rect = self.image.get_rect()
         self.rect.center = (x*100+50,y*100+50)
+        self.type='PAWN'
         self.move2=True
 
         if self.colour == 1:
@@ -98,14 +148,12 @@ class pawn(Piece):
 
         newx=self.posx-1
         newy=self.posy+self.mod
-        if self.check_valid_location(board,newx,newy,'PAWN'):
+        if self.check_valid_location(board,newx,newy):
             dots.append([newx,newy])
         newx=self.posx+1
-        if self.check_valid_location(board,newx,newy,'PAWN'):
+        if self.check_valid_location(board,newx,newy):
             dots.append([newx,newy])
-        return dots
-    
-        
+        return dots    
 
 class knight(Piece):
     def __init__(self, colour,x,y):
@@ -115,6 +163,7 @@ class knight(Piece):
         self.image=pieces[colour][1]
         self.rect = self.image.get_rect()
         self.rect.center = (x*100+50,y*100+50)
+        self.type='KNIGHT'
 
         if self.colour == 1:
             self.oppcolour=0
@@ -127,30 +176,65 @@ class knight(Piece):
         for i in mods:
             newx=self.posx+i[0]
             newy=self.posy+i[1]
-            if self.check_valid_location(board,newx,newy,'KNIGHT'):
+            if self.check_valid_location(board,newx,newy):
                 dots.append([newx,newy])
         return dots
     
 class bishop(Piece):
-    def __init__(self, colour):
+    def __init__(self, colour,x,y):
         super().__init__()
         self.colour=colour
+        self.posx,self.posy=x,y
         self.image=pieces[colour][2]
         self.rect = self.image.get_rect()
+        self.rect.center = (x*100+50,y*100+50)
+        self.type='BISHOP'
+
+        if self.colour == 1:
+            self.oppcolour=0
+        else:
+            self.oppcolour=1
+
+    def check_possible_moves(self,board):
+        return self.checks_for_lines(board,[[1,1],[1,-1],[-1,1],[-1,-1]])
 
 class rook(Piece):
-    def __init__(self, colour):
+    def __init__(self, colour,x,y):
         super().__init__()
         self.colour=colour
+        self.posx,self.posy=x,y
         self.image=pieces[colour][3]
         self.rect = self.image.get_rect()
+        self.rect.center = (x*100+50,y*100+50)
+        self.type='ROOK'
+        self.moved=False
+
+        if self.colour == 1:
+            self.oppcolour=0
+        else:
+            self.oppcolour=1
+
+    def check_possible_moves(self,board):
+        return self.checks_for_lines(board,[[0,1],[0,-1],[1,0],[-1,0]])
 
 class queen(Piece):
-    def __init__(self, colour):
+    def __init__(self, colour,x,y):
         super().__init__()
         self.colour=colour
+        self.posx,self.posy=x,y
         self.image=pieces[colour][4]
         self.rect = self.image.get_rect()
+        self.rect.center = (x*100+50,y*100+50)
+        self.type='QUEEN'
+
+        if self.colour == 1:
+            self.oppcolour=0
+        else:
+            self.oppcolour=1
+
+    def check_possible_moves(self,board):
+        return self.checks_for_lines(board,[[0,1],[0,-1],[1,0],[-1,0],[1,1],[1,-1],[-1,1],[-1,-1]])
+
 
 class king(Piece):
     def __init__(self, colour,x,y):
@@ -160,6 +244,8 @@ class king(Piece):
         self.image=pieces[colour][5]
         self.rect = self.image.get_rect()
         self.rect.center = (x*100+50,y*100+50)
+        self.type='KING'
+        self.moved=False
 
         if self.colour == 1:
             self.oppcolour=0
@@ -174,6 +260,31 @@ class king(Piece):
                 newy=self.posy+j
                 if self.check_valid_location(board,newx,newy):
                     dots.append([newx,newy])
+        
+        castleL=True
+        for i in range(1,4):
+            if board[self.posy][i]==0:
+                pass
+            else:
+                castleL=False
+                break
+
+        castleR=True
+        for i in range(5,7):
+            if board[self.posy][i]==0:
+                pass
+            else:
+                castleR=False
+                break
+        
+        if not self.moved:
+            if isinstance(board[self.posy][0],Piece) and not board[self.posy][0].moved:
+                if castleL:
+                    dots.append([self.posx-2,self.posy])
+            if isinstance(board[self.posy][7],Piece) and not board[self.posy][7].moved:
+                if castleR:
+                    dots.append([self.posx+2,self.posy])
+
         return dots
         
 
