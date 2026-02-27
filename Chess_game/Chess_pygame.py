7093334ef89ada37f695b95d86594858a06fb52d
PennyGuyDyls <dylanprothero@icloud.com>
2026-02-27 23:56:44 +0000

    Chess is going great.

--- Chess_game/Chess_pygame.py
+++ Chess_game/Chess_pygame.py
@@ -1,5 +1,4 @@
 import pygame
-import copy
 light=(150,255,150)
 dark=(100,255,100)
 black=(0,0,0)
@@ -41,6 +40,8 @@ class game():
         self.whiteincheck=False
         self.red_square=None
         self.moved=False
+        self.checkmate=False
+        self.stalemate=False
 
     def select(self,row,col):
 
@@ -54,8 +55,18 @@ class game():
                 x,y = pos
             else:
                 self.red_square=None
+            game_end=self.check_game_end()
             if self.blackincheck or self.whiteincheck:
                 self.red_square=[x,y]
+                if game_end and self.blackincheck:
+                    self.checkmate='BLACK'
+                elif game_end and self.whiteincheck:
+                    self.checkmate='WHITE'
+            else:
+                if game_end:
+                    self.stalemate=True
+
+                
 
         elif self.board[row][col] == 0:
             self.moved=False
@@ -65,7 +76,7 @@ class game():
         elif isinstance(self.board[row][col],Piece) and self.board[row][col].colour==self.turn:
             self.moved=False
             self.action_piece=self.board[row][col]
-            self.dots=self.board[row][col].check_possible_moves(self.board)
+            self.dots=self.action_piece.check_possible_moves(self.board)
             self.dots=self.action_piece.check_legality(self.board,self.dots)
 
     def swap_turn(self):
@@ -91,6 +102,14 @@ class game():
         self.blackincheck=False
         self.whiteincheck=False
         return None
+    
+    def check_game_end(self):
+        for i in range(8):
+            for j in range(8):
+                if isinstance(self.board[i][j],Piece) and self.board[i][j].colour == self.turn:
+                    if self.board[i][j].check_legality(self.board,(self.board[i][j].check_possible_moves(self.board))) != []:
+                        return False
+        return True
 
 
 
@@ -136,6 +155,10 @@ class Piece(pygame.sprite.Sprite):
         board[self.posy][self.posx]=0
         self.posx,self.posy=x,y
         self.rect.center = (x*100+50,y*100+50)
+        if isinstance(board[y][x],Piece):
+            capture_sound.play()
+        else:
+            move_sound.play()
         board[y][x]=self
         return board
     
@@ -147,9 +170,9 @@ class Piece(pygame.sprite.Sprite):
         elif self.type == 'KING':
             self.moved=True
             if x-self.posx==2:
-                board[self.posy][7].move(board,self.posy,5)
+                board[self.posy][7].logic_move(board,self.posy,5)
             if x-self.posx==-2:
-                board[self.posy][0].move(board,self.posy,3)
+                board[self.posy][0].logic_move(board,self.posy,3)
 
         elif self.type =='ROOK':
             self.moved=True
@@ -187,7 +210,9 @@ class Piece(pygame.sprite.Sprite):
         for i in reversed(range(len(dots))):
             fakeboard=self.clone_board(board)
             fakeboard[self.posy][self.posx].logic_move(fakeboard,dots[i][1],dots[i][0])
-            checked = self.in_check(fakeboard)
+            check_colour=False
+            check_colour=self.in_check(board,check_colour)
+            checked = self.in_check(fakeboard,check_colour)
             if checked:
                 if checked=='BLACK' and self.colour==0:
                     dots.pop(i)
@@ -195,15 +220,15 @@ class Piece(pygame.sprite.Sprite):
                     dots.pop(i)
         return dots
 
-    def in_check(self,board):
+    def in_check(self,board,check_colour):
         for i in range(8):
             for j in range(8):
                 if isinstance(board[i][j],Piece):
                     for k in board[i][j].check_possible_moves(board):
                         if isinstance(board[k[1]][k[0]],king) and board[k[1]][k[0]].colour==board[i][j].oppcolour:
-                            if board[i][j].oppcolour==0:
+                            if board[i][j].oppcolour==0 and check_colour!='WHITE':
                                 return 'BLACK'
-                            else:
+                            elif board[i][j].oppcolour==1 and check_colour!='BLACK':
                                 return 'WHITE'
         return False
 
@@ -391,15 +416,74 @@ class king(Piece):
                 break
         
         if not self.moved:
-            if isinstance(board[self.posy][0],Piece) and not board[self.posy][0].moved:
+            if isinstance(board[self.posy][0],rook) and not board[self.posy][0].moved:
                 if castleL:
                     dots.append([self.posx-2,self.posy])
-            if isinstance(board[self.posy][7],Piece) and not board[self.posy][7].moved:
+            if isinstance(board[self.posy][7],rook) and not board[self.posy][7].moved:
                 if castleR:
                     dots.append([self.posx+2,self.posy])
 
         return dots
-        
+
+
+def menu(start):
+    font=pygame.font.SysFont(None, 130)
+    if start:
+        text=font.render('CHESS',True,(255,255,255))
+    else:
+        surf = pygame.Surface((800,800), pygame.SRCALPHA)
+        pygame.draw.rect(surf, (0,0,0,200), (0,0,800,800))
+        screen.blit(surf, (0,0))
+        text=font.render(chess.checkmate+' WINS',True,(255,255,255))
+    screen.blit(text,(screen.get_width()//2-text.get_width()//2,150))
+    pygame.display.flip()
+    pygame.time.wait(1000)
+
+
+    stcolour=(0,200,0)
+    excolour=(200,0,0)
+    font=pygame.font.SysFont(None, 90)
+    if start:
+        sttext=font.render('START',True,(255,255,255))
+    else:
+        sttext=font.render('RESTART',True,(255,255,255))
+    extext=font.render('LEAVE',True,(255,255,255))
+    while True:
+
+        pygame.draw.rect(screen, stcolour, (50,450, 300,200))
+        screen.blit(sttext,(screen.get_width()//2-sttext.get_width()//2-200,550-sttext.get_height()//2))
+
+        pygame.draw.rect(screen, excolour, (450,450, 300,200))
+        screen.blit(extext,(screen.get_width()//2-extext.get_width()//2+200,550-extext.get_height()//2))
+            
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mx, my = pygame.mouse.get_pos()
+                if 450<=my<=650:
+                    if 50<=mx<=350:
+                        return True
+                    elif 450<=mx<=750:
+                        return False
+            elif event.type == pygame.MOUSEMOTION:
+                mx, my = pygame.mouse.get_pos()
+                if 450<=my<=650:
+                    if 50<=mx<=350:
+                        stcolour=(0,255,0)
+                    else:
+                        stcolour=(0,200,0)
+                    if 450<=mx<=750:
+                        excolour=(255,0,0)
+                    else:
+                        excolour=(200,0,0)
+                else:
+                    stcolour=(0,200,0)
+                    excolour=(200,0,0)
+
 
 def show():
     for i in range(8):
@@ -420,26 +504,36 @@ def show():
     if isinstance(chess.action_piece,Piece):
         screen.blit(chess.action_piece.image,chess.action_piece.rect)
 
-
 def draw_dot(x,y):
     surf = pygame.Surface((100, 100), pygame.SRCALPHA)
     pygame.draw.circle(surf, (grey), (50,50), 20)
     screen.blit(surf, (x*100,y*100))
+
+
+pygame.init()
+pygame.mixer.init()
 
 IMG_DIR='Chess_game/Images/'
 pieces_orig=[[pygame.image.load(f'{IMG_DIR}Piece{i}.{j}.png') for i in range(1,7)] for j in range(1,3)]
 pieces=[[pygame.transform.scale(j,(80,85)) for j in i] for i in pieces_orig]
 for i in range(2):
     pieces[i][0]=pygame.transform.scale(pieces_orig[i][0],(60,70))
+    pieces[i][3]=pygame.transform.scale(pieces_orig[i][3],(70,80))
 
-pygame.init()
+SFX_DIR='Chess_game/Sounds/'
+move_sound = pygame.mixer.Sound(SFX_DIR+'Move.wav')
+move_sound.set_volume(0.4)
+capture_sound = pygame.mixer.Sound(SFX_DIR + 'Capture.wav')
+capture_sound.set_volume(0.8)
+
+
+
 chess=game()
 screen=pygame.display.set_mode((800,800))
 
 
-
 hover = False
-running=True
+running=menu(True)
 while running:
     show()
     pygame.display.flip()
@@ -469,4 +563,12 @@ while running:
             mx,my=pygame.mouse.get_pos()
             if isinstance(chess.action_piece,Piece):
                 chess.action_piece.follow(mx,my)
+
+    if chess.checkmate:
+        show()
+        pygame.display.flip()
+        pygame.time.wait(1500)
+        running=menu(False)
+        chess=game()
+
         
